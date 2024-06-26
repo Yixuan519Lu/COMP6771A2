02fb178ad6df7588913c5db2c7aa4ff257fb6246
yixuan lu <z5577113@ad.unsw.edu.au>
2024-06-25 21:58:26 -0700

    implement 2.44 to 2.46

--- src/filtered_string_view.cpp
+++ src/filtered_string_view.cpp
@@ -1,7 +1,7 @@
 #include "./filtered_string_view.h"
 
 namespace fsv {
-	filter filtered_string_view::default_predicate = [](const char&) { return true; };
+	filter filtered_string_view::default_predicate = [](const char&) noexcept { return true; };
 	filtered_string_view::filtered_string_view()
 	: ptr(nullptr)
 	, str_length(0)
@@ -18,5 +18,20 @@ namespace fsv {
 	: ptr(str)
 	, str_length(std::strlen(str))
 	, str_pred(default_predicate) {}
-
+	filtered_string_view::filtered_string_view(const char* str, filter predicate)
+	: ptr(str)
+	, str_length(std::strlen(str))
+	, str_pred(predicate) {}
+	filtered_string_view::filtered_string_view(const filtered_string_view& other)
+	: ptr(other.ptr)
+	, str_length(other.str_length)
+	, str_pred(other.str_pred) {}
+	filtered_string_view::filtered_string_view(filtered_string_view&& other) noexcept
+	: ptr(other.ptr)
+	, str_length(other.str_length)
+	, str_pred(std::move(other.str_pred)) {
+		other.ptr = nullptr;
+		other.str_length = 0;
+		other.str_pred = default_predicate;
+	}
 } // namespace fsv
