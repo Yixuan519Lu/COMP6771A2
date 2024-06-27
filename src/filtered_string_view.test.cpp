70d3fd8ad85c7be366a2c7a75ea8bcf8b12d72b8
yixuan lu <z5577113@ad.unsw.edu.au>
2024-06-27 04:58:22 -0700

    add test cases for constructors

--- src/filtered_string_view.test.cpp
+++ src/filtered_string_view.test.cpp
@@ -2,6 +2,41 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("filter me if you can") {
-	REQUIRE(false);
+using namespace fsv;
+
+TEST_CASE("Default Constructor") {
+	auto sv = filtered_string_view{};
+	REQUIRE(sv.size() == 0);
+}
+
+TEST_CASE("Implicit String Constructor") {
+	auto s = std::string{"cat"};
+	auto sv = filtered_string_view{s};
+	REQUIRE(sv.size() == 3);
+}
+
+TEST_CASE("String Constructor with Predicate") {
+	auto s = std::string{"cat"};
+	auto pred = [](const char& c) { return c == 'a'; };
+	auto sv = filtered_string_view{s, pred};
+	REQUIRE(sv.size() == 1);
+}
+
+TEST_CASE("Implicit Null-Terminated String Constructor") {
+	auto sv = filtered_string_view{"cat"};
+	REQUIRE(sv.size() == 3);
+}
+
+TEST_CASE("Null-Terminated String with Predicate Constructor") {
+	auto pred = [](const char& c) { return c == 'a'; };
+	auto sv = filtered_string_view{"cat", pred};
+	REQUIRE(sv.size() == 1);
+}
+
+TEST_CASE("Copy Constructor and Move Constructor") {
+	auto sv1 = filtered_string_view{"bulldog"};
+	const auto copy = sv1;
+	REQUIRE(copy.data() == sv1.data());
+	const auto move = std::move(sv1);
+	REQUIRE(sv1.data() == nullptr);
 }
